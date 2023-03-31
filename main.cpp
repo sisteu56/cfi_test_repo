d269602844a7892b1c589e3c4a743760675ab76d
Simon Rüdiger Steuer <s8sisteu@stud.uni-saarland.de>
2023-03-31 17:34:02 +0200

    feature Goo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 int main() {
 
+  int x = 0;
+
   bool Foo __attribute__((feature_variable("Foo"))) = true;
+
+  bool Goo __attribute__((feature_variable("Goo"))) = true;
+
+  if (Goo) {
+    x = 1;
+  }
 
   if (Foo) {
     return 42;
   } else {
-    return 1;
+    return 1 - x;
   }
 }
