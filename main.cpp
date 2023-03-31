6881a28b5944ef492a538c78fac7c60f10f2865b
Simon Rüdiger Steuer <s8sisteu@stud.uni-saarland.de>
2023-03-31 11:03:43 +0200

    FeatureB

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 int main(int argc, char *argv[]) {
     static const bool FeatureA = std::string(argv[1]) == "--FeatureA";
+    static const bool FeatureB = std::string(argv[2]) == "--FeatureB";
     if (FeatureA) {
         std::cout << "FeatureA is activated" << std::endl;
+        if (FeatureB) {
+        std::cout << "FeatureB is activated" << std::endl;
+        }
     }
     return 0;
 }
