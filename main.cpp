4e96851049a54111ed4ba290e7a94ba702728ac9
Simon Rüdiger Steuer <s8sisteu@stud.uni-saarland.de>
2023-03-31 10:32:42 +0200

    FeatureTest

--- main.cpp
+++ main.cpp
@@ -2,4 +2,10 @@
 #include <string>
 #include <unordered_map>
 
-int main(int argc, char *argv[]) {}
+int main(int argc, char *argv[]) {
+    static const bool FeatureA = std::string(argv[1]) == "--FeatureA";
+    if (FeatureA) {
+        std::cout << "FeatureA is activated" << std::endl;
+    }
+    return 0;
+}
