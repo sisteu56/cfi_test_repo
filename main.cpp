91a74ebf7719421f086feaaf589b617656d65eb9
Simon Rüdiger Steuer <s8sisteu@stud.uni-saarland.de>
2023-03-31 15:30:27 +0200

    working Feature vars

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
+int main() {
 
-int main(int argc, char *argv[]) {
-    static const bool FeatureA = std::string(argv[1]) == "--FeatureA";
-    static const bool FeatureB = std::string(argv[2]) == "--FeatureB";
-    if (FeatureA) {
-        std::cout << "FeatureA is activated" << std::endl;
-        if (FeatureB) {
-        std::cout << "FeatureB is activated" << std::endl;
-        }
-    }
-    return 0;
+  bool Foo __attribute__((feature_variable("Foo"))) = true;
+
+  if (Foo) {
+    return 42;
+  } else {
+    return 1;
+  }
 }
