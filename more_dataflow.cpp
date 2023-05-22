4b80dfbee32c97afce805a353521d109a578300d
Simon Rüdiger Steuer <s8sisteu@stud.uni-saarland.de>
2023-05-22 10:51:05 +0200

    impl feature vars

--- more_dataflow.cpp
+++ more_dataflow.cpp
@@ -1,3 +1,7 @@
 int main(int argc, char** argv) {
 
+    bool __attribute__((feature_variable("FeatureA"))) FeatureA = argv[0] == "--A";
+    bool __attribute__((feature_variable("FeatureB"))) FeatureB = argv[1] == "--B";
+    bool __attribute__((feature_variable("FeatureC"))) FeatureC = argv[2] == "--C";
+
 }
